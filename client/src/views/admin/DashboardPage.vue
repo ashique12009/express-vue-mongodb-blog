0429bcc97e76a23eada01e1b4aff252ceb04efb5
ashique <ashique12009@gmail.com>
2024-08-31 18:16:46 +0300

    dashboard data

--- client/src/views/admin/DashboardPage.vue
+++ client/src/views/admin/DashboardPage.vue
@@ -29,9 +29,9 @@
             </div>
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-danger text-white mb-4">
-                    <div class="card-body">Danger Card</div>
+                    <div class="card-body">Total Comments</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="#">View Details</a>
+                        <span class="text-white">{{ totalComments }}</span>
                     </div>
                 </div>
             </div>
@@ -47,6 +47,7 @@ const userEmail = ref('')
 const totalPostsNumber = ref(0)
 const totalUsersNumber = ref(0)
 const totalPostNumberOfToday = ref(0)
+const totalComments = ref(0)
 
 const getTotalPostsNumner = async () => {
     try {
